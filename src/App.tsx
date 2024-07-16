e3581b1ef36bce344bef7a18907e9a6d85f870a4
Bekmyrza uulu Janarbek <99035622+zhanarkadyrmatov@users.noreply.github.com>
2024-07-16 14:39:58 +0600

    janarbek

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="text-3xl text-[#61dafb] font-bold underline">Hello World</h1>
     </div>
   );
 }
